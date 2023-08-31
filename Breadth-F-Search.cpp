0421bb64f35da39fed9c0e7e0a0e4a9b482a76bc
waqa26r <115084710+waqa26r@users.noreply.github.com>
2023-08-31 10:58:44 +0530

    Save

--- Breadth-F-Search.cpp
+++ Breadth-F-Search.cpp
@@ -79,6 +79,104 @@ int main()
 
 
 
+//------Lets try With Recursive Function--------
+
+/*
+#include <bits/stdc++.h>
+#include <unordered_map>
+using namespace std;
+
+void BFS_Recursive(vector<int> adj[], vector<bool>& visited, queue<int>& q)
+{
+    if (q.empty())
+        return;
+
+    int u = q.front();
+    q.pop();
+    cout << u << " ";
+
+    for (int vertex : adj[u])
+    {
+        if (!visited[vertex])
+        {
+            visited[vertex] = true;
+            q.push(vertex);
+        }
+    }
+
+    BFS_Recursive(adj, visited, q);
+}
+
+void BFS(vector<int> adj[], vector<bool>& visited, int source)
+{
+    queue<int> q;
+
+    visited[source] = true;
+    q.push(source);
+
+    BFS_Recursive(adj, visited, q);
+}
+
+int main()
+{
+    int vertex, edges;
+    cout << "Enter the number of vertices: ";
+    cin >> vertex;
+    cout << "Enter the number of edges: ";
+    cin >> edges;
+
+    vector<int> adj[vertex]; // Create an adjacency list for the graph
+
+    cout << "Enter the edges (format: u v):" << endl;
+    for (int i = 0; i < edges; i++)
+    {
+        int u, v;
+        cin >> u >> v;
+        adj[u].push_back(v);
+    }
+
+    vector<bool> visited(vertex, false);
+    int source;
+    cout << "Enter the source vertex: ";
+    cin >> source;
+
+    BFS(adj, visited, source);
+
+    return 0;
+}
+
+
+*/
+
+
+/*
+Output:
+
+Enter the number of vertices: 6
+Enter the number of edges: 7
+Enter the edges (format: u v):
+0 1
+0 2
+1 3
+2 3
+3 4
+4 5
+5 0
+Enter the source vertex: 0
+0 1 2 3 4 5 
+
+
+*/
+
+
+
+
+
+
+//---------------------------Coding ninja problem using map---------------------------------------------------
+
+
+
 
 /*
 
