36e725e84d2a0f25f12e2fede2242882791997c7
waqa26r <115084710+waqa26r@users.noreply.github.com>
2023-08-31 10:18:39 +0530

    ss

--- Breadth-F-Search.cpp
+++ Breadth-F-Search.cpp
@@ -1,4 +1,84 @@
 
+ 
+// ---- In this Data Structure of graph what we gonna do that...
+// Whenever we jump to next node  we will search all possible connected of current node then we move forward
+
+
+//The Breadth-First Search (BFS) traversal explores a graph level by level, starting from a source node. 
+// It visits all immediate neighbors before moving to their unvisited neighbors, 
+// making it useful for finding the shortest path and discovering connections in a graph.
+
+
+
+
+
+
+
+//----  This one is simple BSF----
+
+/**
+
+#include <bits/stdc++.h>
+#include <unordered_map>
+using namespace std;
+
+void BFS(vector<int> adj[], vector<bool> &visited, int source)
+{
+    queue<int> q;
+
+    visited[source] = true;
+    q.push(source);
+
+    while (!q.empty())
+    {
+        int u = q.front();
+        q.pop();
+        cout << u << " ";
+
+        for (int vertex : adj[u])
+        {
+            if (!visited[vertex])
+            {
+                visited[vertex] = true;
+                q.push(vertex);
+            }
+        }
+    }
+}
+
+int main()
+{
+    int vertex, edges;
+    cout << "Enter the number of vertices: ";
+    cin >> vertex;
+    cout << "Enter the number of edges: ";
+    cin >> edges;
+
+    vector<int> adj[vertex]; // Create an adjacency list for the graph
+
+    cout << "Enter the edges (format: u v):" << endl;
+    for (int i = 0; i < edges; i++)
+    {
+        int u, v;
+        cin >> u >> v;
+        adj[u].push_back(v);
+    }
+
+    vector<bool> visited(vertex, false);
+    int source;
+    cout << "Enter the source vertex: ";
+    cin >> source;
+
+    BFS(adj, visited, source);
+
+    return 0;
+}
+
+*/
+
+
+
+
 
 /*
 
